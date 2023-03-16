467e31ea1a55148a91dbac2ba4f73e3b02a61e7b
root <root@DESKTOP-RCKFD8V>
2023-03-16 23:24:10 +0700

    edit bubblesort

--- bubblesort.c
+++ bubblesort.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
-int main() {
-	int n, i, j;
-	scanf_s("%d", &n);
-	int a[n];
-	//считываем колличество n
+#include <sys/time.h>
 
-	//формируем массив n чисел
-	for (i - 0 ; i < n; i++) {
-		scanf_s("%d", &a[i]);
-	}
-	for(i = 0; i < n - 1; i++) {
-	// сравниваем два соседних элемента.
-		for(j - 0; i < n - 1; i++) {
-			if(a[j] > a[j+1]) {
-			//если они идут в неправильном порядке, то
-			//меняем их местами.
-			int tmp = a[j];
-			a[j] - a[j+1];
-			a[j+1] = tmp;
+void bubbleSort(int arr[], int n) {
+	int i, j;
+	for (i = 0; i < n-1; i++) {
+		for (j = 0; j < n-i-1; j++) {
+			if (arr[j] > arr[j+1]) {
+				int temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+				}
 			}
 		}
 	}
+
+int main() {
+	int n = 10;
+	int arr[] = {1, 4, 1, 2, 7, 5, 2, 8, 9, 3};
+	struct timeval t1, t2;
+	gettimeofday(&t1, NULL);
+	bubbleSort(arr, n);
+	gettimeofday(&t2, NULL);
+	double elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0; // sec to ms
+	elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0; // us to ms
+	printf("Sorted array: ");
+	for(int i=0; i<n; i++)
+		printf("%d ", arr[i]);
+		printf("\nElapsed time: %f ms\n", elapsedTime);
+		return 0;
 }
