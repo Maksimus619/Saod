7a690e302c77d9751bcc0028e1eb39dec0843f44
Maksimus619 <124022097+Maksimus619@users.noreply.github.com>
2023-03-31 13:12:45 +0700

    Update Count.c
    add comments

--- Count.c
+++ Count.c
@@ -6,10 +6,10 @@ double wtime()
 {
 struct timeval t;
 gettimeofday(&t, NULL);
-return (double)t.tv_sec + (double)t.tv_usec * 1E-6;
+return (double)t.tv_sec + (double)t.tv_usec * 1E-6; // 1E-6 служит для возведение в -6 степень (*10^-6), а функция получает время в микросекундах и при возврате переводит их в секунды с точностью до миллисекунд
 }
 
-int getrand(int min, int max)
+int getrand(int min, int max) // функция для генерации случайных чисел в заданном диапазоне min и max
 {
 return (double)rand() / (RAND_MAX + 1.0) * (max - min) + min;
 }
@@ -17,12 +17,12 @@ return (double)rand() / (RAND_MAX + 1.0) * (max - min) + min;
 int main()
 {
         int x;
-	scanf("%d",&x);
-	double n=wtime();
+	scanf("%d",&x); // считываем
+	double n = wtime(); // присваем переменной n значение полученное из wtime
         int mass[x], max=0, min=0, max1=100000;
         for(int i = 0; i < x; i++)
         {
-                mass[i]= getrand(min, max1);
+                mass[i]= getrand(min, max1); // генерация случайних чисел для заполнения массива и нахождения максимального значенич
                 if (max<mass[i]){max=mass[i];}
         }
 	max++;
@@ -36,7 +36,7 @@ int main()
         {
                 mass1[mass[i]] += 1;
         }
-		double m=wtime();
+// 		double m=wtime(); // получение конечного времени выполнения программы
 		printf("%lf \n",m-n);
         return 0;
 }
