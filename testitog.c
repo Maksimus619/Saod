d84c684d1e582f6638fedd5604f4043e4502d164
Maksimus619 <124022097+Maksimus619@users.noreply.github.com>
2023-12-22 03:37:34 +0700

    Update testitog.c

--- testitog.c
+++ testitog.c
@@ -12,7 +12,158 @@ struct Node {
 
 typedef struct Node Node;
 
-// ... (оставшаяся часть кода без изменений)
+typedef struct {
+    Node* node;
+    int level;
+} SearchResult;
+
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->level = 1;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+Node* rotateRight(Node* root) {
+    if (root && root->left) {
+        Node* left = root->left;
+        root->left = left->right;
+        left->right = root;
+        return left;
+    }
+    return root;
+}
+
+Node* rotateLeft(Node* root) {
+    if (root && root->right) {
+        Node* right = root->right;
+        root->right = right->left;
+        right->left = root;
+        right->level++;
+        return right;
+    }
+    return root;
+}
+
+Node* skew(Node* root) {
+    if (root && root->left && root->level == root->left->level) {
+        root = rotateRight(root);
+    }
+    return root;
+}
+
+Node* split(Node* root) {
+    if (root && root->right && root->right->right && root->right->right->level == root->level) {
+        root = rotateLeft(root);
+    }
+    return root;
+}
+
+Node* findMin(Node* node) {
+    while (node->left) {
+        node = node->left;
+    }
+    return node;
+}
+
+Node* deleteNode(Node* root, int data) {
+    if (!root) {
+        return root;
+    }
+
+    if (data < root->data) {
+        root->left = deleteNode(root->left, data);
+    } else if (data > root->data) {
+        root->right = deleteNode(root->right, data);
+    } else {
+        if (!root->left || !root->right) {
+            Node* temp = root->left ? root->left : root->right;
+            if (!temp) {
+                temp = root;
+                root = NULL;
+            } else {
+                *root = *temp;
+            }
+            free(temp);
+        } else {
+            Node* temp = findMin(root->right);
+            root->data = temp->data;
+            root->right = deleteNode(root->right, temp->data);
+        }
+    }
+
+    root = skew(root);
+    root = split(root);
+    return root;
+}
+
+Node* insert(Node* root, int data) {
+    if (!root) {
+        return createNode(data);
+    }
+
+    if (data < root->data) {
+        root->left = insert(root->left, data);
+    } else if (data > root->data) {
+        root->right = insert(root->right, data);
+    }
+
+    root = skew(root);
+    root = split(root);
+    return root;
+}
+
+void inOrder(Node* root) {
+    if (root) {
+        inOrder(root->left);
+        printf("%d ", root->data);
+        inOrder(root->right);
+    }
+}
+
+bool isNodePresent(Node* root, int data) {
+    if (!root) {
+        return false;
+    }
+
+    if (data < root->data) {
+        return isNodePresent(root->left, data);
+    } else if (data > root->data) {
+        return isNodePresent(root->right, data);
+    }
+
+    return true;
+}
+
+SearchResult search(Node* root, int data, int currentLevel) {
+    SearchResult result;
+    result.node = NULL;
+    result.level = 0;
+
+    if (!root) {
+        return result;
+    }
+
+    if (data < root->data) {
+        return search(root->left, data, currentLevel + 1);
+    } else if (data > root->data) {
+        return search(root->right, data, currentLevel + 1);
+    } else {
+        result.node = root;
+        result.level = currentLevel;
+        return result;
+    }
+}
+
+int countNodes(Node* root) {
+    if (!root) {
+        return 0;
+    }
+
+    return 1 + countNodes(root->left) + countNodes(root->right);
+}
 
 int main() {
     Node* root = NULL;
@@ -21,15 +172,20 @@ int main() {
     // Инициализация генератора случайных чисел
     srand((unsigned int)time(NULL));
 
-    // Засекаем время перед выполнением операций
-    struct timespec start_time, end_time;
-    clock_gettime(CLOCK_MONOTONIC, &start_time);
-
     // Генерация и добавление случайных значений
     for (int i = 0; i < values_count; i++) {
         int random_value = rand() % (values_count * 10);
         root = insert(root, random_value);
     }
+
+    // Засекаем время перед выполнением операций
+    clock_t start_time = clock();
+
+    printf("In-order traversal of the AA tree: ");
+    inOrder(root);
+    printf("\n");
+
+    printf("Number of nodes in the tree: %d\n", countNodes(root));
 
     // Добавление случайного значения
     int random_insert_value = rand() % (values_count * 10);
@@ -49,9 +205,25 @@ int main() {
         printf("Element not found.\n");
     }
 
+    // Поиск определенного значения
+    int search_value = 42; // Замени это значение на то, которое ты хочешь найти в дереве
+    SearchResult search_result = search(root, search_value, 1);
+
+    if (search_result.node) {
+        printf("Element with value %d found at level %d.\n", search_value, search_result.level);
+    } else {
+        printf("Element with value %d not found in the tree.\n", search_value);
+    }
+
+    printf("In-order traversal after operations: ");
+    inOrder(root);
+    printf("\n");
+
+    printf("Number of nodes in the tree after operations: %d\n", countNodes(root));
+
     // Засекаем время после выполнения операций
-    clock_gettime(CLOCK_MONOTONIC, &end_time);
-    double elapsed_time = (end_time.tv_sec - start_time.tv_sec) * 1000.0 + (end_time.tv_nsec - start_time.tv_nsec) / 1000000.0;
+    clock_t end_time = clock();
+    double elapsed_time = ((double)(end_time - start_time) * 1000) / CLOCKS_PER_SEC;
 
     printf("Прошедшее время: %.2f миллисекунд\n", elapsed_time);
 
